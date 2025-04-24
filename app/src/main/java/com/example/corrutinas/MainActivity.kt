8ad8c12716fa975478d1c2e167fe415dd4824b26
Daniel <dalmusterpereira@danielcastelao.org>
2025-04-24 16:51:26 +0200

    Cuenta atras y botones F y V

--- app/src/main/java/com/example/corrutinas/MainActivity.kt
+++ app/src/main/java/com/example/corrutinas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,11 +17,16 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.corrutinas.ui.theme.CorrutinasTheme
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
@@ -43,24 +49,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun JuegoFrases(name: String, modifier: Modifier = Modifier) {
     var tiempo by remember { mutableStateOf(20) }
+    val Corrutina = rememberCoroutineScope()
+
     Column {
         Row {
             Button(
                 onClick = {
-                    repeat {  }
-                    runBlocking {
-                        delay(1000)
-                        tiempo -= 1
+                    tiempo = 20
+                    Corrutina.launch {
+                        repeat(20) {
+                            delay(1000)
+                            tiempo -= 1
+                        }
                     }
-                },
+                }
             ) {
-                Text(
-                    text = "Start"
-                )
+                Text("Start")
             }
             Text(
-                text = tiempo.toString()
+                text = "$tiempo",
+                modifier = modifier
+                    .border(
+                        width = 2.dp,
+                        color = Color.Black
+                    )
+                    .padding(horizontal = 20.dp)
             )
+            Button(
+                onClick = {
+
+                }
+            ) {
+               Text("V")
+            }
+            Button(
+                onClick = {
+
+                }
+            ) {
+                Text("F")
+            }
         }
     }
 }
